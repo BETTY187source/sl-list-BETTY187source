8fd57d60b4d3bc41d3f444ba723798615330d478
Benie <bbenimanabetty@gmail.com>
2026-02-17 01:18:03 -1000

    Add SLinkedList.java

--- SLinkedList.java
+++ SLinkedList.java
@@ -51,16 +51,18 @@ public class SLinkedList<T> implements Iterable<T>{
         tail=newNode;
         size++;
     }
-    public void addLast(T data){
+    public void addLast(T data) {
         Node<T> newNode = new Node<>(data);
         if (head == null) {
             head = newNode;
-            tail=newNode;
+            tail = newNode;
         } else {
             tail.next = newNode;
+            tail = newNode; // ✅ move tail forward to the new node
         }
         size++;
     }
+    
     public int size(){
         return size;
     }
@@ -135,18 +137,98 @@ public class SLinkedList<T> implements Iterable<T>{
         }
         return tail.data;
     }
-    public void reverse(){
-        
-    //TODO
+    public void remove(T e) {
+        if (head == null) {
+            return; // list is empty, nothing to remove
+        }
+    
+        // Case 1: element is at the head
+        if (head.data.equals(e)) {
+            head = head.next;
+            size--;
+            if (head == null) {
+                tail = null; // list became empty
+            }
+            return;
+        }
+    
+        // Case 2: element is somewhere else
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(e)) {
+                // found the node to remove
+                current.next = current.next.next;
+                size--;
+                if (current.next == null) {
+                    tail = current; // removed the last node
+                }
+                return;
+            }
+            current = current.next;
+        }
     }
+    
+    public void reverse() {
+        if (head == null || head.next == null) {
+            return; // nothing to reverse if list is empty or has one element
+        }
+    
+        Node<T> prev = null;
+        Node<T> current = head;
+        tail = head; // after reversal, the old head becomes the tail
+    
+        while (current != null) {
+            Node<T> nextNode = current.next; // store next node
+            current.next = prev;             // reverse the link
+            prev = current;                  // move prev forward
+            current = nextNode;              // move current forward
+        }
+    
+        head = prev; // new head is the last non-null node
+        // TO-DO
+    }
+    
     public void deleteConsecutiveDuplicates(){
+        if (head == null) return;
+
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.data.equals(current.next.data)) {
+                current.next = current.next.next;
+                size--;
+            } else {
+                current = current.next;
+            }
+        }
+
+        tail = current;
        //TODO
     }
     //two lists are equal if they have the same 
     // size and the same elements in the same order
     @Override
     public boolean equals(Object obj){
-        //TODO
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        SLinkedList<?> other = (SLinkedList<?>) obj;
+
+        if (this.size != other.size) return false;
+
+        Node<T> current1 = this.head;
+        SLinkedList<T>.Node<?> current2 = (SLinkedList<T>.Node<?>)other.head;
+
+        while (current1 != null) {
+            if (!current1.data.equals(current2.data)) {
+                return false;
+            }
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+
+        return true;
     }
+
+        //TODO
 
 }
